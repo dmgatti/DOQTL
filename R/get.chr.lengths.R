e13d0e2a5649023f0cb65c501d7c2275e581fbd6
Daniel Gatti <Dan.Gatti@jax.org>
2015-07-13 12:26:09 +0000

    Commit made by the Bioconductor Git-SVN bridge.
    Commit id: 1f8433d428a3abf783bcac7eeef799f595ab00cd
    
        Fixed get.chr.lengths bug.
    
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/DOQTL@106030 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/get.chr.lengths.R
+++ R/get.chr.lengths.R
@@ -2,14 +2,14 @@
 # Get the mouse chromosome lengths.
 get.chr.lengths = function(genome = "mm10") {
 
-  bsgenome = get(paste0("BSgenome.Mmusculus.UCSC.", genome))
-
-  chrlen = seqlengths(bsgenome)
-  chrlen = chrlen[-grep("random|Un", names(chrlen))]
-  names(chrlen) = sub("chr", "", names(chrlen))
-  chrlen = chrlen[c(1:19, "X", "Y", "M")]
-  chrlen = chrlen * 1e-6
-  
-  return(chrlen)
+    genome = get(paste0("BSgenome.Mmusculus.UCSC.", genome))
+    
+    chrlen = seqlengths(genome)
+    chrlen = chrlen[-grep("random|Un", names(chrlen))]
+    names(chrlen) = sub("chr", "", names(chrlen))
+    chrlen = chrlen[c(1:19, "X", "Y", "M")]
+    chrlen = chrlen * 1e-06
+    
+    return(chrlen)
   
 } # get.chr.lengths()
