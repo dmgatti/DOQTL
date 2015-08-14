22d5673b9326a0f117cae4c1b21e23c259438ab6
dmgatti <Dan.Gatti@jax.org>
2015-08-14 16:20:09 -0400

    Fixed bug on X chromosome.

--- R/bayesint.R
+++ R/bayesint.R
@@ -12,36 +12,43 @@
 #            expandtomarkers: boolean, expand the QTL interval to the outermost
 #                 flanking markers if TRUE.  Default = FALSE.
 bayesint = function(qtl, chr, prob = 0.95, expandtomarkers = FALSE) {
+
   if(missing(qtl)) {
     stop("bayesint: The qtl cannot be null. Please supply a QTL object.")
   } # if(is.null(qtl))
+  
   if(missing(chr)) {
     stop(paste("bayesint: The chromosome cannot be null."))
   } else if(!chr %in% c(1:19, "X")) {
     stop(paste("bayesint: The chromosome must be 1 to 19 or X."))
   } # else if
+  
   if(prob < 0 | prob > 1) {
    stop(paste("bayesint: The probability must between 0 and 1."))
   } # if(prob < 0 | prob > 1)
+  
   old.warn = options("warn")$warn
   options(warn = -1)
-  if(is.numeric(as.numeric(chr))) {
+  if(!is.na(as.numeric(chr))) {
     qtl = qtl$lod$A
   } else {
     qtl = qtl$lod$X
   } # else
   options(warn = old.warn)
+  
   qtl[,1] = as.character(qtl[,1])
   qtl[,2] = as.character(qtl[,2])
   qtl[,3] = as.numeric(qtl[,3])
   qtl[,7] = as.numeric(qtl[,7])
   qtl = qtl[qtl[,2] == chr,]
   pos = qtl[,3]
+  
   if(any(is.na(pos))) {
     remove = which(is.na(pos))
     qtl = qtl[-remove,]
     pos = pos[-remove]
   } # if(any(is.na(pos)))
+  
   # Make a set of 10,000 intervals so that we can integrate numerically.
   breaks = approx(x = pos, y = 10^qtl[,7], xout = seq(pos[1], pos[length(pos)],
                   length.out = 1e5))
