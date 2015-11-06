376a8b71684326af924c39851ddf695dd3f98824
Daniel Gatti <Dan.Gatti@jax.org>
2015-11-06 15:44:59 +0000

    Commit made by the Bioconductor Git-SVN bridge.
    Commit id: 47a7fed597306392cba3ba710eba761fc9b824a2
    
        Added get.pgw.
    
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/DOQTL@110400 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/get.pgw.R
@@ -0,0 +1,49 @@
+################################################################################
+# Given a set of permutations and a LOD or -log10(p-value), caluclate the
+# genome wide p-value.
+# Daniel Gatti
+# dan.gatti@jax.org
+# Nov. 5, 2015
+################################################################################
+get.pgw = function(stat, chr, perms) {
+
+  if(length(stat) != length(chr)) {
+
+    stop("The length of the statistics must be the same as the length of the",
+         "chromosomes.")
+
+  } # if(length(stat) != length(chr))
+
+  if(min(stat) >= 0 & max(stat) <= 1) {
+    warning("The values in stat are all between 0 and 1. If they are p-values,
+            please convert them to -log10(p-values).")
+  } # if(min(stat) >= 0 & max(stat) <= 1)
+
+  if(min(perms) >= 0 & max(perms) <= 1) {
+    warning("The values in perms are all between 0 and 1. If they are p-values,
+            please convert them to -log10(p-values).")
+  } # if(min(perms) >= 0 & max(perms) <= 1)
+
+  # Get the chromosome lengths.
+  chrlen = get.chr.lengths()
+  len.auto = sum(chrlen[1:19])
+  len.X = chrlen["X"]
+  len.all = len.auto + len.X
+
+  # Set up the return value.
+  pgw = rep(1, length(stat))
+  names(pgw) = names(stat)
+
+  isX = chr == "X"
+
+  # Autosomes
+  q = sapply(lapply(stat[!isX], "<", perms[,"A"]), mean, na.rm = TRUE)
+  pgw[!isX] = 1.0 - (1.0 - q)^(len.all / len.auto)  
+
+  # X chromosome.
+  q = sapply(lapply(stat[isX], "<", perms[,"X"]), mean, na.rm = TRUE)
+  pgw[isX] = 1.0 - (1.0 - q)^(len.all / len.X)  
+
+  return(pgw)
+
+} # get.pgw()
