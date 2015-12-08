c5a05ce390f0755a0c9c9f9691a0a76b359c2704
dmgatti <Dan.Gatti@jax.org>
2015-12-08 09:24:21 -0500

    Added get.genotype() to get the Sanger SNP allele calls from association mapping. Fixed a get.max.geno() bug where it was looping over the wrong dimension.

--- R/get.max.geno.R
+++ R/get.max.geno.R
@@ -26,21 +26,26 @@ get.max.geno = function(probs) {
     for(s in 1:nrow(probs)) {
   
       if(s %% 50 == 0) print(s)
-      mxp  = apply(probs[s,,,drop = FALSE], 1, max)
-      wmxp = apply(probs[s,,,drop = FALSE], 1, which.max)
+      mxp  = apply(probs[s,,,drop = FALSE], 3, max)
+      wmxp = apply(probs[s,,,drop = FALSE], 3, which.max)
       homo = which(mxp >= 0.75)
-      max.geno[s,homo] = paste(founders[wmxp[homo]], founders[wmxp[homo]], sep = "")
+      if(length(homo) > 0) {
+        max.geno[s, homo] = paste(founders[wmxp[homo]], founders[wmxp[homo]], sep = "")
+      } # if(length(homo) > 0)
       het = which(mxp < 0.75)
-      rnk = apply(probs[s,,het,drop = FALSE], 1, rank)
-      rnk = apply(rnk, 2, ">", 6)
-      rnk = apply(rnk, 2, which)
-      let = matrix("", nrow(rnk), ncol(rnk))
-      let[1,] = founders[rnk[1,]]
-      let[2,] = founders[rnk[2,]]
-      let = apply(let, 2, sort)
-      max.geno[s,het] = paste(let[1,], let[2,], sep = "")
+      if(length(het) > 0) {
+        rnk = apply(probs[s,,het,drop = FALSE], 3, rank)
+        rnk = apply(rnk, 2, ">", 6)
+        rnk = apply(rnk, 2, which)
+        let = matrix("", nrow(rnk), ncol(rnk))
+        let[1,] = founders[rnk[1,]]
+        let[2,] = founders[rnk[2,]]
+        let = apply(let, 2, sort)
+        max.geno[s, het] = paste(let[1,], let[2,], sep = "")
+      } # if(length(het) > 0)
     
     } # for(s)
+
   } # else
   
   return(max.geno)
