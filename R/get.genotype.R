cd36e496b5a2f8c0c3ef575c0a83483048409829
Daniel Gatti <Dan.Gatti@jax.org>
2015-12-08 14:32:06 +0000

    Commit made by the Bioconductor Git-SVN bridge.
    Commit id: 3040977c3277350c7b899a47198e5a9bacd4161a
    
        Fixed version
    
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/DOQTL@111301 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/get.genotype.R
@@ -0,0 +1,40 @@
+################################################################################
+# Given the haplotype probabilities and the location of a single SNP,
+# use the haplotype probabilities and the SNP to impute the allele calls in 
+# DO mice.
+# Daniel Gatti
+# Dan.Gatti@jax.org
+# Dec. 7, 2015
+################################################################################
+# Arguments: chr: a single character (or number) that is the chromosome.
+#            pos: a positive value that is the SNP location in bp of Mb.
+#            snp: The strain pattern of the SNP as 0s and 1s. Must be a named
+#                 vector with the strain names.
+#            probs: 3D array containing the haplotype probabilities.
+#            markers: The location of the markers in probs.
+get.genotype = function(chr, pos, snp, probs) {
+
+  # See if the position is in bp or Mb.
+  if(pos < 200) {
+    pos = pos * 1e6
+  } # if(pos < 200)
+
+  # Convert the SNP to numbers.
+  snp = unlist(snp)
+  names(snp) = make.names(sub("_", ".", names(snp)))
+  strains = make.names(do.colors[,2])
+  snp = snp[strains]
+  snp = as.numeric(factor(snp)) - 1
+
+  # Get the slices from the haplotype probs matrix.
+  markers = markers[markers[,2] == chr,]
+  markers = markers[which.min(markers[,3] >= pos * 1e-6):which.max(markers[,3] <= pos * 1e-6),]
+
+  # Get the probs for these markers.
+  probs = probs[,,markers[,1], drop = FALSE]
+  probs = apply(probs, 1:2, mean)
+
+  # Multiply the two matrices and return the result.
+  return(probs %*% snp)
+
+} # get.genotype()
