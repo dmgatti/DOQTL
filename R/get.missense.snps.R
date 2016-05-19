d092750dd2703c250c651bf155510f4e0cb31bc6
Daniel Gatti <Dan.Gatti@jax.org>
2016-05-19 10:17:09 -0400

    Create get.missense.snps.R

---
+++ R/get.missense.snps.R
@@ -0,0 +1,12 @@
+################################################################################
+# Given a VCF object from VariantAnnotation readVCF(), extract the SNPs that
+# Daniel Gatti
+# dan.gatti@jax.org
+# May 19, 2016
+################################################################################
+get.missense.snps = function(vcf) {
+
+  csq = info(vcf)$CSQ
+  
+
+} # get.missense.snps()
